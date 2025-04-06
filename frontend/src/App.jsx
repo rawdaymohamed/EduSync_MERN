6c72360fa87f35665817fb6b2688dd0c1b5c1a4a
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-06 04:03:04 +0200

    Feat: add dummy pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/student/Home";
+import CourseList from "./pages/student/CourseList";
+import CourseDetails from "./pages/student/CourseDetails";
+import MyEnrollments from "./pages/student/MyEnrollments";
+import Player from "./pages/student/Player";
+import Loading from "./components/student/Loading";
+import Educator from "./pages/educator/Educator";
+import Dashboard from "./pages/educator/Dashboard";
+import AddCourse from "./pages/educator/AddCourse";
+import MyCourses from "./pages/educator/MyCourses";
+import StudentsEnrolled from "./pages/educator/StudentsEnrolled";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-2xl">Hello world</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<CourseList />} />
+        <Route path="/courses/:input" element={<CourseList />} />
+        <Route path="/course/:courseId" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/courses/:courseId/player" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+        <Route path="/educator" element={<Educator />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="new-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="students-enrolled" element={<StudentsEnrolled />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
+import { AppContextProvider } from "./context/AppContext.jsx";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>
 );
